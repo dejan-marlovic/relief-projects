4ae492e354ca831e21226cdcc8d51bed7aad33a4
dejan-marlovic <dmarlovic83@gmail.com>
2025-04-30 17:49:04 +0200

    Added modular scss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
-import Login from "./pages/Login";
+import Login from "./pages/Login/Login";
 import Transactions from "./pages/Transactions";
 import Budgets from "./pages/Budgets";
 import Project from "./pages/Project";
