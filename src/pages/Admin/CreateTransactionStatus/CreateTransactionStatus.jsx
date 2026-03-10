1a007e5ce94b512c02ab841590c7e9c60ac8b5a7
dejan-marlovic <dmarlovic83@gmail.com>
2026-03-10 13:14:38 +0100

    working on create transaction status admin component

--- src/pages/Admin/CreateTransactionStatus/CreateTransactionStatus.jsx
+++ src/pages/Admin/CreateTransactionStatus/CreateTransactionStatus.jsx
@@ -11,6 +11,36 @@ import {
   extractFieldErrors,
 } from "../../../utils/http";
 
+//never changed we copy it into state tranasctionStatusDetails once.
 const intialTransactionStatusDetails = {
   transactionStatusName: "",
+};
+
+const validateTransactionStatusDetails = (values) => {
+  const errors = {};
+
+  const name = values.transactionStatusName?.tim() || "";
+
+  if (!name) {
+    errors.transactionStatusName =
+      "Transaction status name is required (e.g. Payment made)";
+  }
+
+  return errors;
+};
+
+const CreateTransactionStatus = () => {
+  const navigate = useNavigate();
+  //we save the authFetch call and create new only if useNavigate changes
+  const authFetch = useMemo(authFetch(useNavigate), [useNavigate]);
+
+  //UI states
+  const [loading, setLoading] = useState(false);
+  const [formError, setFormError] = useState("");
+  const [fieldError, setFieldError] = useState({});
+
+  //Form state
+  const [transactionStatusDetails, setTransactionStatusDetails] = useState(
+    intialTransactionStatusDetails,
+  );
 };
